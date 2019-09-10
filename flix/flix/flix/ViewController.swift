42aa489dcdba364d02fa6ee1314c9c898f5e020f
Elijah Baird <elijah_baird84@yahoo.com>
2019-09-09 18:28:05 -0700

    Created flix project

---
+++ flix/flix/flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Elijah Baird on 9/9/19.
+//  Copyright © 2019 Elijah Baird. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
