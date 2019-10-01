d49a49eca1b64cd96f4891634f69efe6aa34db17
Elijah Baird <elijah_baird84@yahoo.com>
2019-10-01 02:20:44 -0700

    Finished Twitter assignment. git was no longer tracking the other folder so now all content has been moved from twitter_ios_starter to twitter

---
+++ twitter/Twitter/TweetTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Elijah Baird on 9/30/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var tweetLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
